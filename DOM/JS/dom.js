225448e7e2f64679b5ac3c41872f973d6c620ed0
kdhsea <kdhsea@gmail.com>
2021-01-22 17:03:28 +0900

    front - practice dom

---
+++ DOM/JS/dom.js
@@ -0,0 +1,5 @@
+var newParagraph = document.createElement("p");
+var paragraphContent = document.createTextNode("I'm new!");
+
+newParagraph.appendChild(paragraphContent);
+document.body.appendChild(newParagraph);
